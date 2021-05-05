af4af127b87741435b08dcc0f81edf47a41cadfc
zabiullasheriff <71973129+zabiullasheriff@users.noreply.github.com>
2021-05-05 11:20:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-const data 
-=
-fetch("https://dog.ceo/api/breeds/image/random")
-.then(function(response)) {
-return response.json()
-}).then(function(jsonData) {
-    console.log(jsonData)
+fetch('https://dog.ceo/api/breeds/list/all')
+.then(function(response){
+    return response.json()
 })
+.then(function(data){
+    createBreedList(data.message)
+})  
+
+function createBreedList(breedList){
+    document.getElementById("breed").innerHTML = `
+    <select onchange="showByBreed(this.value)">
+            <option>Select a breed</option>
+            ${Object.keys(breedList).map(function(breed){
+                return `<option>${breed}</option>`
+            }).join('')}
+        </select>
+    `
+}
+function showByBreed(breed){
+    if (breed != 'Select a breed'){
+        fetch(`https://dog.ceo/api/breed/${breed}/images`)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            displayImage(data.message)
+        })
+    }
+}
+function displayImage(images){
+    document.getElementById('pic').innerHTML = `
+    <div class="picture" style="background-image: url('${images[0]}');"></div>
+    `
+}
